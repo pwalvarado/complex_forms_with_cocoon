d6e1461e9b43c0a9ec0c24e28b2517a3e0ae3bac
Pedro Alvarado <pwalvarado@gmail.com>
2015-04-03 10:01:35 -0500

    Setting up relations in models and nested attributes

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -1,5 +1,11 @@
 class Quantity < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :ingredient
-  attr_accessible :amount
+
+  attr_accessible :amount, 
+                  :ingredient,
+                  :ingredient_attributes
+  
+  accepts_nested_attributes_for :ingredient, 
+                                :reject_if => :all_blank
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ActiveRecord::Base
-  attr_accessible :description, :instructions, :title
+  attr_accessible :description, :instructions, :title,
+                  :quantities_attributes
+  
+  has_many :quantities
+  has_many :ingredients, 
+           :through => :quantities
+  
+  accepts_nested_attributes_for :quantities, 
+           :reject_if => :all_blank, 
+           :allow_destroy => true
+  accepts_nested_attributes_for :ingredients
 end
