3f44216c8062aae956691945df7cb530e4972f00
Pedro Alvarado <pwalvarado@gmail.com>
2015-04-03 09:41:05 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ComplexFormsWtihCocoon::Application.config.secret_token = '47960e3a32f7e2803eed71396e3d1817046379653763342cb0e49d526331739108269edcd8d912e89670ca8ca2ef36513af131e6a873fcb25cafc51eddd134e3'
